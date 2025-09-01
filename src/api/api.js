9ef1cab8178f7540bbe8218c1aa08181518befa4
Rodrigolima1508 <rodrigofelicianolima@gmail.com>
2025-09-01 00:45:45 -0300

    Corrigido erro da barra dupla na URL da API

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+const baseURL = 'https://projeto-coletores.onrender.com';
 
 const api = axios.create({
   baseURL: `${baseURL}/api`,
